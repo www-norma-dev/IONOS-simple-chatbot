8207d9cc244c030fe07a4b259451bc2ac1eb8c2d
alaahouidheg <alahouidheg@gmail.com>
2025-09-10 16:34:11 +0100

    fix build

--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 import Link from "next/link";
 import type { GetStaticProps } from "next";
 import { JSX } from "react";
-import { PlayCircle, PauseCircle } from "lucide-react";
+import { PlayCircle } from "lucide-react";
 import { useRef, useState } from "react";
 
 type Props = {
